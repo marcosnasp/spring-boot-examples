9dcee84507348d5c1f86d1974dc137d070bc7328
Marcos Portela <marcosnasp@gmail.com>
2018-04-19 13:51:08 -0300

    Inclusão de casos de testes de integração

--- spring-boot-2-jdbc-with-h2/src/main/java/com/in28minutes/springboot/jdbc/h2/example/student/StudentJdbcRepository.java
+++ spring-boot-2-jdbc-with-h2/src/main/java/com/in28minutes/springboot/jdbc/h2/example/student/StudentJdbcRepository.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class StudentJdbcRepository {
+	
 	@Autowired
 	JdbcTemplate jdbcTemplate;
 
--- spring-boot-2-jdbc-with-h2/src/test/java/com/in28minutes/springboot/jdbc/h2/example/SpringBoot2JdbcWithH2ApplicationTests.java
+++ spring-boot-2-jdbc-with-h2/src/test/java/com/in28minutes/springboot/jdbc/h2/example/SpringBoot2JdbcWithH2ApplicationTests.java
@@ -1,16 +1,69 @@
 package com.in28minutes.springboot.jdbc.h2.example;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
-@RunWith(SpringRunner.class)
+import com.in28minutes.springboot.jdbc.h2.example.student.Student;
+import com.in28minutes.springboot.jdbc.h2.example.student.StudentJdbcRepository;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@JdbcTest
 @SpringBootTest
+@ContextConfiguration(classes = TestRepositoryConfiguration.class, 
+		loader = AnnotationConfigContextLoader.class)
 public class SpringBoot2JdbcWithH2ApplicationTests {
-
+	
+	@Autowired
+	StudentJdbcRepository studentRepo;
+	
 	@Test
 	public void contextLoads() {
 	}
 
+	@Test
+	public void selectOneSpecificStudentTest() {
+		final Student studentLocal = new Student(10001L, "Ranga", "E1234567");
+
+		final long id = 10001;
+		final Student student = studentRepo.findById(id);
+
+		assertThat(student).isNotNull();
+		assertThat(studentLocal).isEqualToComparingFieldByField(student);
+	}
+
+	@Test
+	public void selectAllStudentsTest() {
+		final List<Student> allStudents = studentRepo.findAll();
+		
+		assertThat(allStudents).isNotEmpty();
+		assertThat(allStudents).hasSize(2);
+	}
+	
+	@Test
+	public void deleteOneStudentById() {
+		final int rowsAffected = 1;
+		final long id = 10001;
+		
+		assertThat(studentRepo.deleteById(id)).isEqualTo(rowsAffected);
+		assertThat(studentRepo.findAll()).hasSize(1);
+	}
+	
+	@Test
+	public void insertOneStudentTest() {
+		final Student studentLocal = new Student(10003L, "Michael", "E12345679");
+
+		int rowsAffected = studentRepo.insert(studentLocal);
+		assertThat(rowsAffected).isEqualTo(1);
+	}
+	
 }
