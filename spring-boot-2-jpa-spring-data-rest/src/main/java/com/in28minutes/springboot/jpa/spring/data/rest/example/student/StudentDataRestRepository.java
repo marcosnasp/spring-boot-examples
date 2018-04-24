5ac5ed1b8cd9434fa8f24775212691bb248e31a1
Marcos Portela <marcosnasp@gmail.com>
2018-04-24 14:28:30 -0300

    Inclusão de testes integração para o web service (REST)

--- spring-boot-2-jpa-spring-data-rest/src/main/java/com/in28minutes/springboot/jpa/spring/data/rest/example/student/StudentDataRestRepository.java
+++ spring-boot-2-jpa-spring-data-rest/src/main/java/com/in28minutes/springboot/jpa/spring/data/rest/example/student/StudentDataRestRepository.java
@@ -1,9 +1,18 @@
 package com.in28minutes.springboot.jpa.spring.data.rest.example.student;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
 
 @RepositoryRestResource(path = "students", collectionResourceRel = "students")
 public interface StudentDataRestRepository extends PagingAndSortingRepository<Student, Long>{
+
+    Student findStudentById(@Param("id") Long id);
+
+    List<Student> findStudentsByName(@Param("name") String name);
+
+    Student findStudentByPassportNumber(@Param("pass") String pass);
 
 }
--- spring-boot-2-jpa-spring-data-rest/src/test/java/com/in28minutes/springboot/jpa/spring/data/rest/example/SpringBoot2JPAWithSpringDataRestApplicationTests.java
+++ spring-boot-2-jpa-spring-data-rest/src/test/java/com/in28minutes/springboot/jpa/spring/data/rest/example/SpringBoot2JPAWithSpringDataRestApplicationTests.java
@@ -1,16 +1,85 @@
 package com.in28minutes.springboot.jpa.spring.data.rest.example;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.equalToIgnoringWhiteSpace;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = SpringBoot2JPAWithSpringDataRestApplication.class)
 public class SpringBoot2JPAWithSpringDataRestApplicationTests {
+
+    private final int port = 8080;
+
+    private TestRestTemplate restTemplate = new TestRestTemplate();
+    private HttpHeaders headers = new HttpHeaders();
+
+    @Before
+    public void init() {
+    }
 
 	@Test
 	public void contextLoads() {
 	}
+
+    @Test
+    public void findAllStudentsRequestTest() {
+        HttpEntity<String> entity = new HttpEntity<>(null, headers);
+
+        ResponseEntity<String> response = getStringResponseEntity(entity, "/students/");
+
+        assertEquals(response.getStatusCode(), HttpStatus.OK);
+    }
+
+    @Test
+    public void findAllStudentsWithAssuredRestTest() {
+        final String RESPONSE = "{ \"name\" : \"Ravi\", \"passportNumber\" : \"A1234568\", " +
+                "\"_links\" : { \"self\" : { \"href\" : \"http://localhost:8080/students/10002\" }, " +
+                "\"student\" : { \"href\" : \"http://localhost:8080/students/10002\" } } }";
+
+        final Response response = RestAssured.given()
+                .baseUri(createURLWithPort("/students/"))
+                .when().get("10002");
+
+        response.then().assertThat().statusCode(equalTo(200));
+        assertThat(response.getBody().asString(),
+                equalToIgnoringWhiteSpace(RESPONSE));
+
+    }
+
+    @Test
+    public void findOneStudentByIdTest() throws Exception {
+
+        final String RESPONSE = "{name: Ravi, passportNumber: A1234568, " +
+                "_links: { self: { href: 'http://localhost:8080/students/10002'}, " +
+                "student: { href: 'http://localhost:8080/students/10002'}}}";
+
+        HttpEntity<String> entity = new HttpEntity<>(null, headers);
+        ResponseEntity<String> response = getStringResponseEntity(entity, "/students/10002");
+
+        assertEquals(response.getStatusCode(), HttpStatus.OK);
+        JSONAssert.assertEquals(RESPONSE, response.getBody(), false);
+
+    }
+
+    private ResponseEntity<String> getStringResponseEntity(HttpEntity<String> entity, String url) {
+        return restTemplate.exchange(createURLWithPort(url),
+                HttpMethod.GET, entity, String.class);
+    }
+
+    private String createURLWithPort(String uri) {
+        return "http://localhost:" + port + uri;
+    }
 
 }
